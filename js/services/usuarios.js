94892a85c74acecd4e295ecada0b531ee7e1ae8a
Cognicion <aldousaquila@gmail.com>
2026-06-24 18:02:15 -0600

    Update usuarios.js

--- js/services/usuarios.js
+++ js/services/usuarios.js
@@ -1,1 +1,61 @@
+import { db } from "../firebase.js";
 
+import {
+    doc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    collection,
+    getDocs,
+    query,
+    where
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+export async function obtenerUsuario(uid){
+
+    const snap = await getDoc(
+        doc(db,"usuarios",uid)
+    );
+
+    if(!snap.exists()) return null;
+
+    return snap.data();
+
+}
+
+
+
+export async function listarPacientes(){
+
+    const q = query(
+        collection(db,"usuarios"),
+        where("rol","==","paciente")
+    );
+
+    return await getDocs(q);
+
+}
+
+
+
+export async function actualizarUsuario(uid,datos){
+
+    await updateDoc(
+        doc(db,"usuarios",uid),
+        datos
+    );
+
+}
+
+
+
+export async function crearUsuario(uid,datos){
+
+    await setDoc(
+        doc(db,"usuarios",uid),
+        datos
+    );
+
+}
