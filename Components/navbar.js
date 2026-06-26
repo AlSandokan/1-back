4b03f952b498aa881caee8e3681ad3c6c4cbbcf5
Cognicion <aldousaquila@gmail.com>
2026-06-26 12:42:07 -0600

    Create navbar.js

---
+++ Components/navbar.js
@@ -0,0 +1,64 @@
+import { auth } from "../firebase.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const navbar = document.getElementById("navbar");
+
+onAuthStateChanged(auth, (user) => {
+
+    navbar.innerHTML = `
+
+    <nav class="navbar-medico">
+
+        <div class="logo">
+
+            Cognición
+
+        </div>
+
+        <div class="menu">
+
+            <a href="dashboard.html">
+                🏠 Inicio
+            </a>
+
+            <a href="medico.html">
+                👨‍⚕️ Panel médico
+            </a>
+
+            <button id="btnVolver">
+
+                ← Regresar
+
+            </button>
+
+            <button id="btnSalir">
+
+                Cerrar sesión
+
+            </button>
+
+        </div>
+
+    </nav>
+
+    `;
+
+    document
+        .getElementById("btnVolver")
+        .onclick = () => history.back();
+
+    document
+        .getElementById("btnSalir")
+        .onclick = async () => {
+
+            await signOut(auth);
+
+            location.href = "login.html";
+
+        };
+
+});
